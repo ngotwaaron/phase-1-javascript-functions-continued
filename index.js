346f38d6ea37826f276b0ff721c24cf51340f73d
ngotwaaron <ngotwaaron@gmail.com>
2024-03-22 11:58:48 +0300

    complete phase-1- functions continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const newActivity = "bathe my dog";
+  console.log(saturdayFun(newActivity));
+  console.log(saturdayFun()); 
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+  const newMonday =  "work from home";
+  console.log(mondayWork(newMonday));
+  console.log(mondayWork());
+  
+  function wrapAdjective(noun) {
+    return function(adjective) {
+      return `You are ${noun}${adjective}${noun}!`;
+    }
+  }
+  
